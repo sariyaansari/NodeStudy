fc310d7cbd947d2dc328d1c604c484078c4094bc
Sariya Ansari <sariyaansari@gmail.com>
2017-03-14 17:35:40 +0530

    Added todos item in web-server with GET

--- src/web-server.js
+++ src/web-server.js
@@ -3,10 +3,29 @@ var middleware = require('./middleware.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+		id:1,
+		description: 'Need to learn Node.js first',
+		completed: false
+	    },
+	    {
+		id:2,
+		description: 'Develop Application after that',
+		completed: false
+	    },
+            {
+		id:3,
+		description: 'Publish the application',
+		completed: false
+	    }];
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
 	res.send('About Us');
+});
+
+app.get('/todos', function(req, res) {
+	res.json(todos);
 });
 
 app.use(express.static(__dirname + '/../public'));
