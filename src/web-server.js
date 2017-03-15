511ff5d7c3f612befbf5de09de93344458bea734
Sariya Ansari <sariyaansari@gmail.com>
2017-03-16 00:26:31 +0530

    Implemented http GET with multiple query params for todo API

--- src/web-server.js
+++ src/web-server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 3000;
 var todos = [{
 		id:1,
 		description: 'Need to learn Node.js first',
-		completed: false
+		completed: true
 	    },
 	    {
 		id:2,
@@ -38,6 +38,7 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 
 /** Get all todos using HTTP GET /todos*/
 /** Get todos based on query e.g. /todos?completed=true */
+/** Get todos based on query e.g. /todos?completed=false&q=work*/
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -46,6 +47,14 @@ app.get('/todos', function(req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if (queryParams.hasOwnProperty('q') &&
+			_.isString(queryParams.q) &&
+			queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q) > -1;
+		});
 	}
 	res.json(filteredTodos);
 });
