e458f92d4d7af79219ae2c4e19d85c1e5c7e6560
Sariya Ansari <sariyaansari@gmail.com>
2017-03-16 21:56:07 +0530

    defining sql schema with validations & adding, fetching & printing all records

--- sqliteTestSrc/basic-sqlite-database.js
+++ sqliteTestSrc/basic-sqlite-database.js
@@ -10,22 +10,59 @@ Define model (means schema)
 */
 var Todo = sequelize.define('todo', {
 	description: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false, //cannot contain null as a string
+		validate: {
+			len: [1, 100]
+		}
 	},
 	completed: {
-		type:Sequelize.BOOLEAN
-	} 
+		type:Sequelize.BOOLEAN,
+		defaultValue: true
+	}
 });
 
 sequelize.sync().then(function() {
 	console.log('Everything is synced');
-	
+
+  //finding specific id and printing the row
+	Todo.findById(3).then(function(todo){
+		if (todo){
+			console.log(todo.toJSON());
+		} else {
+			console.log('Item not exists in table');
+		}
+	});
+
 	//Add row in table created above
 	Todo.create({
-		description: "First Row Added",
-		completed: false
+		description: "I am with new row",
+		completed: true
 	}).then(function (todo){
-		console.log('Data Added successfully');
-		console.log(todo);
+		return Todo.create({
+			//Adding another row using promise
+			 description: "Clean my personal cabin"
+		});
+	}).then(function() {
+		//Then findAll records where completed is true & description contains 'new'
+		return Todo.findAll({
+			where: { 
+				completed: true,
+				description:{
+					$like: '%new%'
+				}
+			}
+		});
+	}).then(function (todos){
+		//now finally print all records that matches the above criteria
+		if (todos) {
+			todos.forEach(function(todo){
+				console.log(todo.toJSON());
+			});
+		}
+	}).catch(function(e) {
+		//Catch and exit gracefully if schema defination violated
+		console.log(e);
 	});
+
 });
