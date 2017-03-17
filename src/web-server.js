41acc5070643c00925bd97c4774e9ce16daa28db
Sariya Ansari <sariyaansari@gmail.com>
2017-03-17 11:24:15 +0530

    Reverting web-server changes to handle TODO API with sqlite

--- src/web-server.js
+++ src/web-server.js
@@ -2,29 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var middleware = require('./middleware.js');
-var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [
-/*							{
-								id:1,
-								description: 'Need to learn Node.js first',
-								completed: true
-							},
-					    {
-								id:2,
-								description: 'Develop Application after that',
-								completed: false
-					    },
-	            {
-								id:3,
-								description: 'Publish the application',
-								completed: false
-					    }
-*/
-		];
+var todos = [{
+		id:1,
+		description: 'Need to learn Node.js first',
+		completed: true
+	     },
+	     {
+		id:2,
+		description: 'Develop Application after that',
+		completed: false
+	     },
+	     {
+		id:3,
+		description: 'Publish the application',
+		completed: false
+	    }];
 
 var todoCurrId = todos.length;
 
@@ -82,38 +78,31 @@ app.post('/todos', function(req, res) {
 	// correct key-value pair
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function(todo){
-		res.json(todo.toJSON());
-	}, function(err){
-		res.status(400).send();
-	});
+ 	if (!_.isBoolean(body.completed) ||
+ 	    !_.isString(body.description) ||
+ 	    body.description.trim().length === 0) {
+ 		return res.status(400).send();
+ 	}
 
+ 	body.description = body.description.trim(); //trim leading and trailing blanks in description value
+ 	body.id = ++todoCurrId;
+ 	todos.push(body);
 
-// 	if (!_.isBoolean(body.completed) ||
-// 	    !_.isString(body.description) ||
-// 	    body.description.trim().length === 0) {
-// 		return res.status(400).send();
-// 	}
-//
-// 	body.description = body.description.trim(); //trim leading and trailing blanks in description value
-// 	body.id = ++todoCurrId;
-// 	todos.push(body);
-//
-// 	console.log('description ' + body.description);
-// 	res.json(todos[todoCurrId-1]);
-// });
-//
-// /** delete todos item using HTTP delete */
-// app.delete('/todos/:id', function(req, res){
-// 	var todoId = parseInt(req.params.id, 10);
-// 	var matchedTodo = _.findWhere(todos, {id: todoId});
-//
-// 	if (!matchedTodo) {
-// 		 res.status(404).json({"info": "Item not found"});
-// 	} else {
-// 		todos = _.without(todos, matchedTodo);
-// 		res.json(matchedTodo);
-//  }
+	console.log('description ' + body.description);
+	res.json(todos[todoCurrId-1]);
+ });
+
+ /** delete todos item using HTTP delete */
+ app.delete('/todos/:id', function(req, res){
+ 	var todoId = parseInt(req.params.id, 10);
+ 	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+ 	if (!matchedTodo) {
+ 		 res.status(404).json({"info": "Item not found"});
+ 	} else {
+ 		todos = _.without(todos, matchedTodo);
+ 		res.json(matchedTodo);
+  }
 });
 
 /** Update specific 'id' data using HTTP Put */
@@ -145,10 +134,7 @@ app.put('/todos/:id', function(req,res) {
 
 app.use(express.static(__dirname + '/../public'));
 
-
-db.sequelize.sync().then(function(){
-	//TODO : why to listen here
-	app.listen(PORT, function(req,res){
-		console.log('Server Running on port:' + PORT);
-	});
+app.listen(PORT, function(req,res){
+	console.log('Server Running on port:' + PORT);
 });
+
