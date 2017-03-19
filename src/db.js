643855d2dca4ebdbff6723ea52fe8f8012048235
Sariya Ansari <sariyaansari@gmail.com>
2017-03-19 15:20:10 +0530

    Added postgres DB sequelizer for production

--- src/db.js
+++ src/db.js
@@ -1,8 +1,16 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname .substr(0, __dirname.length-4)+ '/data/data-todo-API.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+if (evn === 'production') {
+  //On heroku server this object will be used
+  sequelize = new Sequelize(process.env.DATABASE_URL, {'dialect': 'postgres',});
+} else {
+  //on localhost this object will be used
+  sequelize = new Sequelize(undefined, undefined, undefined, {
+  'dialect': 'sqlite',
+  'storage': __dirname .substr(0, __dirname.length-4)+ '/data/data-todo-API.sqlite'
+  });
+}
 
 //creating empty object
 var db = {};
